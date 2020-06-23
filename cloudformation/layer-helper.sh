5b1f5d9ae19d2bce3243ff8eee1e614252d5e2de
px267 <px267@cummins.com>
2020-06-23 15:19:32 -0400

    Automated Layer Deployment

--- cloudformation/layer-helper.sh
+++ cloudformation/layer-helper.sh
@@ -3,6 +3,7 @@
 echo "Getting latest layer versions from the System Manager Parameter Store and replacing it in the parameter files . . ."
 paramFile=$1
 echo "Param File: ${paramFile}"
+# environment=$(echo $paramFile| cut -d'-' -f 2)
 layerNames=$(python -c 'import json;layer_names=json.load(open("layers.json"));keys=list(layer_names.keys());vals=list(layer_names.values());print(" ".join([i+"~"+j for i,j in zip(keys, vals)]))')
 echo $layerNames
 for layerName in $layerNames;
@@ -10,9 +11,19 @@ do
     realLayerName=$(echo $layerName| cut -d'~' -f 2)
     cftParamName=$(echo $layerName| cut -d'~' -f 1)
     echo "current layer: ${realLayerName}"
+    aws configure set profile.stage.role_arn arn:aws:iam::732927748536:role/da-edge-j1939-services-CodeBuildRole-stage
     latestLayerResults=$(aws lambda list-layer-versions --layer-name "${realLayerName}")
-    echo "Latest layer results: ${latestLayerResults}"
-    latestLayerARN=$(echo $latestLayerResults | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["LayerVersions"][0]["LayerVersionArn"])')
-    echo "Latest layer ARN: ${latestLayerARN}"
-    echo "${cftParamName}*-*${latestLayerARN}*-*${paramFile}" | python -c 'import json,sys;layer_obj=sys.stdin.read();layer_names=json.load(open("layers.json"));param_values=layer_obj.strip().split("*-*");parameter_file=json.load(open(param_values[2]));parameter_file["Parameters"][param_values[0]]=param_values[1];json.dump(parameter_file, open(param_values[2], "w"))'
+    echo "Latest layer results - Dev: ${latestLayerResults}"
+
+    aws configure set profile.stage.role_arn arn:aws:iam::170736887717:role/da-edge-j1939-services-cloudformationdeployer-stage-role
+    latestLayerResults=$(aws lambda list-layer-versions --layer-name "${realLayerName}")
+    echo "Latest layer results - Dev: ${latestLayerResults}"
+
+
+    # latestLayerARN=$(echo $latestLayerResults | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["LayerVersions"][0]["LayerVersionArn"])')
+    # echo "Latest layer ARN: ${latestLayerARN}"
+    # latestLayerVersion=${latestLayerARN##*:}
+    # aws lambda remove-layer-version-permission --layer-name "${realLayerName}" --version-number "${latestLayerVersion}" --statement-id "${realLayerName}_layer_CA_permission_SID"
+    # aws lambda add-layer-version-permission --layer-name "${realLayerName}" --statement-id "${realLayerName}_layer_CA_permission_SID" --action lambda:GetLayerVersion --principal 170736887717 --version-number "${latestLayerVersion}" --output json
+    # echo "${cftParamName}*-*${latestLayerARN}*-*${paramFile}" | python -c 'import json,sys;layer_obj=sys.stdin.read();layer_names=json.load(open("layers.json"));param_values=layer_obj.strip().split("*-*");parameter_file=json.load(open(param_values[2]));parameter_file["Parameters"][param_values[0]]=param_values[1];json.dump(parameter_file, open(param_values[2], "w"))'
 done;
