5e5b12bc26633914575abf17d0ca20eb39415dce
px938 <px938@cummins.com>
2020-08-18 11:09:04 -0400

    EDGE-2551 Changes

--- EdgeCPPTPoster/pt_poster.py
+++ EdgeCPPTPoster/pt_poster.py
@@ -1,28 +1,26 @@
 import json
 import boto3
 import requests
+import traceback
 
 
 def send_to_pt(post_url, headers, json_body):
 
     try:
         headers_json = json.loads(headers)
-
         final_json_body = [json_body]
-
         print("Posting the JSON body:", final_json_body, "to the PT Cloud through URL:",
               post_url, "with headers:", headers_json)
 
-        pt_response = requests.post(url=post_url, data=json_body, headers=headers)
+        pt_response = requests.post(url=post_url, data=json.dumps(final_json_body), headers=headers_json)
 
         print("Get device info response as text:", pt_response.text)
 
         pt_response_body = pt_response.json()
         pt_response_code = pt_response.status_code
-
         print("Get device info response code:", pt_response_code)
         print("Get device info response body:", pt_response_body)
 
     except Exception as e:
-
+        traceback.print_exc()
         print("ERROR! An exception occurred while posting to PT endpoint:", e)
