9ff86b913c5f6f0c79782caaa656bda2c26e3f64
px938 <px938@cummins.com>
2020-08-19 14:54:00 -0400

    Adding PT header to secret manager

--- EdgeCPPTPoster/pt_poster.py
+++ EdgeCPPTPoster/pt_poster.py
@@ -3,11 +3,29 @@ import boto3
 import requests
 import traceback
 
+secret_name = os.environ['pt_xapi_key']
+
+# Create a Secrets Manager client
+session = boto3.session.Session()
+sec_client = session.client(
+    service_name='secretsmanager',
+    region_name=region_name
+)
+
 
 def send_to_pt(post_url, headers, json_body):
 
     try:
         headers_json = json.loads(headers)
+        get_secret_value_response = sec_client.get_secret_value(SecretId=secret_name)
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+            print("Secret Value Response Secret",secret)
+            secret = json.loads(secret)
+            api_key = secret['x-api-key']
+        else:
+            print("PT x-api-key not exist in secret manager")
+        headers_json['x-api-key'] = api_key
         final_json_body = [json_body]
         print("Posting the JSON body:", final_json_body, "to the PT Cloud through URL:",
               post_url, "with headers:", headers_json)
