3abd9f7e37a2fcb975a45d7d4a6435e65be87c94
PRAVEEN MOGANTI <py929@cummins.com>
2020-10-07 15:23:59 -0400

    code fix

--- EdgeCPPTPoster/update_scheduler.py
+++ EdgeCPPTPoster/update_scheduler.py
@@ -7,9 +7,8 @@ from pypika import Query, Table, Order, functions as fn
 def update_scheduler_table(req_id):
     print('updating scheduler table')
     query = get_update_scheduler_query(req_id)
-    print('query',query)
+    print(query)
     try:
-        print('enter try')
         edge.api_request(os.environ["EdgeCommonDBAPI"], "post", query)
         print('Successfully updated scheduler table')
     except Exception as exception:
@@ -17,7 +16,6 @@ def update_scheduler_table(req_id):
         return edge.server_error(str(exception)) 
 
 def get_update_scheduler_query(req_id):
-    print('get updating scheduler query')
     scheduler = Table('da_edge_olympus.scheduler')
     query = Query.update(scheduler).set(scheduler.status, 'Data Rx in progress').where(scheduler.request_id == req_id)
     return query.get_sql(quote_char=None)
