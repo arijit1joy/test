52b777c253d80c9d6b315f7de9d42fed86cc54ae
PRAVEEN MOGANTI <py929@cummins.com>
2020-10-15 11:17:00 -0400

    Changes

--- EdgeCPPTPoster/tests/test.py
+++ EdgeCPPTPoster/tests/test.py
@@ -1,15 +1,20 @@
 import unittest
 from unittest import mock
 from unittest.mock import patch
-import update_scheduler
 
+
+with patch.dict('os.environ',
+                {'EdgeCommonDBAPI': 'https://test.edgedb.url',}):
+                import update_scheduler
 
 class TestEdgeCPPTPoster(unittest.TestCase):
 
     def test_update_scheduler_function(self):
-        query = "UPDATE da_edge_olympus.scheduler SET status='Data Rx in progress' WHERE request_id='REQ1233'"
+        query = "UPDATE da_edge_olympus.scheduler SET status='Data Rx in progress' WHERE request_id='REQ1233' AND device_id='102900000000003' AND status='Config Accepted'"
         req_id = 'REQ1233'
-        expected_query = update_scheduler.get_update_scheduler_query(req_id)
+        device_id = '102900000000003'
+        expected_query = update_scheduler.get_update_scheduler_query(req_id, device_id)
+        print('query',query)
         print('expected_query',expected_query)
         assert query == expected_query
 
@@ -18,9 +23,10 @@ class TestEdgeCPPTPoster(unittest.TestCase):
     @patch('update_scheduler.edge.api_request')
     @patch('update_scheduler.get_update_scheduler_query')
     def test_update_scheduler_function_return_success(self, mock_get_update_scheduler_query, mock_api_request):
-        mock_get_update_scheduler_query.return_value = "update da_edge_olympus.scheduler set status = 'Data Rx in progress' where request_id = 'REQ1233'"
+        mock_get_update_scheduler_query.return_value = "update da_edge_olympus.scheduler set status = 'Data Rx in progress' where request_id = 'REQ1233' AND status='Config Accepted'"
         mock_api_request.return_value = "Successfully performed operation"
-        result = update_scheduler.update_scheduler_table('REQ1233')
+        device_id = '102900000000003'
+        result = update_scheduler.update_scheduler_table('REQ1233', device_id)
         mock_get_update_scheduler_query.assert_called()
         mock_api_request.assert_called()
 
@@ -31,6 +37,18 @@ class TestEdgeCPPTPoster(unittest.TestCase):
     def test_update_scheduler_function_return_exception(self, mock_get_update_scheduler_query, mock_api_request):
         mock_get_update_scheduler_query.return_value = Exception
         mock_api_request.side_effect = "Successfully performed operation"
-        result = update_scheduler.update_scheduler_table('REQ1233')
+        device_id = '102900000000003'
+        result = update_scheduler.update_scheduler_table('REQ1233', device_id)
         mock_get_update_scheduler_query.assert_called()
         mock_api_request.assert_called()
+    
+    def test_get_request_id_from_consumption_view_query(self):
+        query = "SELECT request_id FROM da_edge_olympus.edge_data_consumption_vw WHERE data_type='J1939_HB' AND data_config_filename='EDGE_357649070803120_64100016_SC5079' AND config_status='Config Accepted'"
+        data_protocol = 'J1939_HB'
+        data_config_filename = 'EDGE_357649070803120_64100016_SC5079'
+        expected_query = update_scheduler.get_request_id_from_consumption_view_query(data_protocol, data_config_filename)
+        print('query',query)
+        print('expected_query',expected_query)
+        assert query == expected_query
+    
+
--- EdgeCPPTPoster/update_scheduler.py
+++ EdgeCPPTPoster/update_scheduler.py
@@ -6,23 +6,27 @@ from pypika import Query, Table, Order, functions as fn
 db_api_url = os.environ["EdgeCommonDBAPI"]
 
 def get_request_id_from_consumption_view(data_protocol, data_config_filename):
-    data_protocol = 'eFPA' if data_protocol == 'EFPA' else data_protocol
     query = get_request_id_from_consumption_view_query(data_protocol, data_config_filename)
     request_id = ''
     print(query)
     try:
-        request_id = edge.api_request(db_api_url, "get", query)
+        response = edge.api_request(db_api_url, "get", query)
+        print('response', response)
+        if response:
+            request_id = response[0]['request_id']
+            return request_id
         print('Successfully fetch request id from view')
     except Exception as exception:
         print('Failed to fetch request id from consumption view ')
         return edge.server_error(str(exception))
-    return request_id
+    return None
 
 def get_request_id_from_consumption_view_query(data_protocol, data_config_filename):
     data_consumption_vw = Table('da_edge_olympus.edge_data_consumption_vw')
     query = Query.from_(data_consumption_vw).select(data_consumption_vw.request_id).where(
         data_consumption_vw.data_type == data_protocol
-    ).where(data_consumption_vw.data_config_filename == data_config_filename)
+    ).where(data_consumption_vw.data_config_filename == data_config_filename
+    ).where(data_consumption_vw.config_status == 'Config Accepted')
     return query.get_sql(quote_char=None)
 
 def update_scheduler_table(req_id, device_id):
@@ -42,3 +46,4 @@ def get_update_scheduler_query(req_id, device_id):
     query = Query.update(scheduler).set(scheduler.status, 'Data Rx in progress').where(scheduler.request_id == req_id
                                                                                        ).where(scheduler.device_id == device_id
                                                                                        ).where(scheduler.status == 'Config Accepted')
+    return query.get_sql(quote_char=None)
