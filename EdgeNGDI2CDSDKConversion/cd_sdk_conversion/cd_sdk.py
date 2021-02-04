baf54bd155e4d330f0523057fbe7f2e909a389e9
Joshua Imarhiagbe <px267@cummins.com>
2021-02-04 10:51:48 -0500

    EDGE-3885: Duplicate Message ID testing for both FC & HB

--- EdgeNGDI2CDSDKConversion/cd_sdk_conversion/cd_sdk.py
+++ EdgeNGDI2CDSDKConversion/cd_sdk_conversion/cd_sdk.py
@@ -15,16 +15,17 @@ def map_ngdi_sample_to_cd_payload(parameters, fc=False, hb=False):
         "Please, set either the 'fc' or 'hb' parameters to True."
 
     # Get the appropriate template file based on if the "fc" or the "hb" flag is set to True
-
     cd_json_template = json.load(open(f"cd_sdk_conversion/cd_{'fc' if fc else 'hb'}_sdk_payload.json", "r"))
+
     logger.info(f"CD {'FC' if fc else 'HB'} Json Template: {cd_json_template}")
 
     final_cd_payload = {}
 
+    # Populate the CD Payload Template
     for cd_fc_parameter in cd_json_template:
-        if cd_fc_parameter.lower() in parameters:
+        if cd_fc_parameter.lower() in parameters:  # If the parameter is provided in the current file populate it
             final_cd_payload[cd_fc_parameter] = parameters[cd_fc_parameter.lower()]
-        else:
+        else:  # Send the template's default empty value (e.g. {}, "", [], etc.)
             final_cd_payload[cd_fc_parameter] = cd_json_template[cd_fc_parameter]
 
     return final_cd_payload
--- EdgeNGDI2CDSDKConversion/cd_sdk_conversion/cd_snapshot_sdk.py
+++ EdgeNGDI2CDSDKConversion/cd_sdk_conversion/cd_snapshot_sdk.py
@@ -10,19 +10,24 @@ def get_snapshot_data(params, time_stamp, address, spn_file_json):
 
     logger.info(f"SPN File as JSON: {spn_file_json}")
     try:
+        # For each parameter, add an object to the Snapshot object
         for param in params:
-            parameters.append({
-                "Name": spn_file_json[param],
-                "Value": params[param],
-                "Parameter_Source_Address": address
-            })
+            if param in spn_file_json:
+                parameters.append({
+                    "Name": spn_file_json[param],
+                    "Value": params[param],
+                    "Parameter_Source_Address": address
+                })
+            else:  # If the parameter is not part of the EDGE SPN list, print a warning and move on
+                logger.warn(f"The parameter: '{param}' is not in the EDGE SPN list!")
 
         snapshot_data["Parameter"] = parameters
-        snapshot_data_list = [snapshot_data]
+        snapshot_data_list = [snapshot_data]  # Make the snapshot object a list (per the CD specifications)
 
         logger.info(f"Snapshot Data: {snapshot_data_list}")
         return snapshot_data_list
 
-    except Exception as e:
-        logger.error(f"Error! An Exception {e} occurred when getting the snapshot data!")
-        raise e
+    except Exception as get_snapshot_error:
+        # Catch the error and just print the error to the console
+        logger.error(f"Error! An Exception {get_snapshot_error} occurred when getting the snapshot data!")
+        raise get_snapshot_error
