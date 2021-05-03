b4256b930f1fe60a98369aec5002f0ac1a2f59ed
Joshua Imarhiagbe <px267@cummins.com>
2021-05-03 12:33:08 -0400

    EDGE-4242: Modify the FILE_RECEIVED Stage Population Logic in the Uploaded File Processor to retain UUID associated to the S3 Record of any Uploaded File if Any

--- EdgeCPPTPoster/PosterLambda.py
+++ EdgeCPPTPoster/PosterLambda.py
@@ -222,7 +222,7 @@ def retrieve_and_process_file(s3_event_body, receipt_handle):
 
             logger.info(f"Json_body before calling SEND_TO_PT function: {json_body}")
             sqs_message = sqs_message.replace("FILE_RECEIVED", "FILE_SENT")
-            pt_poster.send_to_pt(PTJ1939PostURL, PTJ1939Header, json_body, sqs_message, file_uuid)
+            pt_poster.send_to_pt(PTJ1939PostURL, PTJ1939Header, json_body, sqs_message)
         else:
 
             logger.error(f"Error! The boxApplication value is not recorded in the EDGE DB!")
