0711fdf59265e62448113bb0e6567d4bc9386a53
Nikunj <rv619@cummins.com>
2021-07-30 12:51:55 -0400

    printing proper error and raise exception

--- EdgeCPPTPoster/kafka_producer.py
+++ EdgeCPPTPoster/kafka_producer.py
@@ -1,19 +1,14 @@
-import json
-from kafka import KafkaProducer
-import boto3
-from botocore.exceptions import ClientError
 import os
+import json
+import boto3
 import edge_logger as logging
-logger = logging.logging_framework("EdgeCPPTPoster.Post")
+from kafka import KafkaProducer
+from botocore.exceptions import ClientError
 
-secret_arn = os.environ['mskSecretArn']
-mskcluster_arn = os.environ['mskClusterArn']
-topic_name = os.environ['topicName']
-
-secrets_client = boto3.client("secretsmanager")
-cluster_client = boto3.client('kafka')
+logger = logging.logging_framework("EdgeCPPTPoster.KafkaProducer")
 cluster_response = {}
 secret_response = {}
+
 
 def get_secret_value():
     """Gets the value of a secret.
@@ -21,20 +16,23 @@ def get_secret_value():
     the secret.
     """
     global secret_response
-   
+
     try:
+        secrets_client = boto3.client("secretsmanager")
+        secret_arn = os.environ['mskSecretArn']
         if not secret_response:
             logger.info("Inside get_secret_value() method")
             kwargs = {'SecretId': secret_arn}
             secret_response = secrets_client.get_secret_value(**kwargs)
     except ClientError as e:
         if e.response['Error']['Code'] == 'ResourceNotFoundException':
-            logger.error("The requested secret " + secret_name + " was not found")
+            logger.error("The requested secret was not found")
         elif e.response['Error']['Code'] == 'InvalidRequestException':
             logger.error("The request was invalid due to:", e)
         elif e.response['Error']['Code'] == 'InvalidParameterException':
             logger.error("The request had invalid params:", e)
     return secret_response
+
 
 def get_brokers(mskcluster_arn):
     """Gets the broker list to publish the message 
@@ -43,45 +41,44 @@ def get_brokers(mskcluster_arn):
     logger.info("Inside get_brokers() method")
     global cluster_response
     try:
-        if not cluster_response:    
+        cluster_client = boto3.client('kafka')
+        if not cluster_response:
             cluster_response = cluster_client.get_bootstrap_brokers(ClusterArn=mskcluster_arn)
     except ClientError as e:
         logger.error("Error while getting broker list", e)
     return cluster_response['BootstrapBrokerStringSaslScram'].split(',')
 
-def publish_message(message_object):
 
-    #Get credentials
+def publish_message(message_object):
+    # Get credentials
     logger.info("Inside publish_message()")
     secret_object = get_secret_value()
-    sec=json.loads(secret_object['SecretString'])
-    #print(sec['username'])
-    
-    #Get msk brokers
-    list_brokers = get_brokers(mskcluster_arn)    
+    sec = json.loads(secret_object['SecretString'])
+    # print(sec['username'])
+
+    # Get msk brokers
+    mskcluster_arn = os.environ['mskClusterArn']
+    list_brokers = get_brokers(mskcluster_arn)
     try:
         producer = KafkaProducer(
-        security_protocol='SASL_SSL',
-        sasl_mechanism='SCRAM-SHA-512',
-        bootstrap_servers=list_brokers,
-        sasl_plain_username=sec['username'],
-        sasl_plain_password=sec['password']
+            security_protocol='SASL_SSL',
+            sasl_mechanism='SCRAM-SHA-512',
+            bootstrap_servers=list_brokers,
+            sasl_plain_username=sec['username'],
+            sasl_plain_password=sec['password']
         )
 
-        producer.send(topic_name,str(message_object).encode('utf-8'))
+        topic_name = os.environ['topicName']
+        producer.send(topic_name, str(message_object).encode('utf-8'))
         producer.flush()
         producer.close()
         json_obj = {
-        "response" : "200"
+            "response": "200"
         }
         return json_obj
     except ClientError as e:
         logger.error("Error while publishing the message to cluster", e)
         json_obj = {
-        "response" : "500"
+            "response": "500"
         }
         return json_obj
-
-
-
-   
--- EdgeCPPTPoster/update_scheduler.py
+++ EdgeCPPTPoster/update_scheduler.py
@@ -12,27 +12,26 @@ db_api_url = os.environ["edgeCommonAPIURL"]
 
 def get_request_id_from_consumption_view(data_protocol, data_config_filename):
     query = get_request_id_from_consumption_view_query(data_protocol, data_config_filename)
-    request_id = ''
-    logger.info(f"{query}")
+    logger.info(f"Get Request ID From Consumption View Query: {query}")
     try:
         response = edge.api_request(db_api_url, "get", query)
-        logger.info(f"response: {response}")
+        logger.info(f"Get Request ID From Consumption View Response: {response}")
         if response:
             request_id = response[0]['request_id']
             return request_id
         logger.info(f'Successfully fetch request id from view')
     except Exception as exception:
-        logger.info(f'Failed to fetch request id from consumption view ')
-        return edge.server_error(str(exception))
+        logger.error(f'Failed to fetch request id from consumption view: {exception}')
+        raise exception
     return None
 
 
 # noinspection PyTypeChecker
 def get_request_id_from_consumption_view_query(data_protocol, data_config_filename):
     data_consumption_vw = Table('da_edge_olympus.edge_data_consumption_vw')
-    query = Query.from_(data_consumption_vw).select(data_consumption_vw.request_id)\
-        .where(data_consumption_vw.data_type == data_protocol)\
-        .where(data_consumption_vw.data_config_filename == data_config_filename)\
+    query = Query.from_(data_consumption_vw).select(data_consumption_vw.request_id) \
+        .where(data_consumption_vw.data_type == data_protocol) \
+        .where(data_consumption_vw.data_config_filename == data_config_filename) \
         .where(data_consumption_vw.config_status == 'Config Accepted')
     return query.get_sql(quote_char=None)
 
@@ -40,10 +39,10 @@ def get_request_id_from_consumption_view_query(data_protocol, data_config_filena
 def update_scheduler_table(req_id, device_id):
     logger.info(f'updating scheduler table')
     query = scheduler.get_update_scheduler_query(req_id, device_id)
-    logger.info(f"{query}")
+    logger.info(f"Updating Scheduler Table Query: {query}")
     try:
         edge.api_request(db_api_url, "post", query)
         logger.info(f'Successfully updated scheduler table')
     except Exception as exception:
-        logger.info(f'Failed to update scheduler table')
-        return edge.server_error(str(exception))
+        logger.error(f'Failed to update scheduler table: {exception}')
+        raise exception
