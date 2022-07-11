8a09b94c60838ed7ea3fced177b0ff64edc292b5
ss350 <ss350@cummins.com>
2022-07-11 11:30:24 -0400

    SPAR-884: refactored connection_utility to remove code duplication

--- EdgeCPPTPoster/utilities/connection_utility.py
+++ EdgeCPPTPoster/utilities/connection_utility.py
@@ -8,33 +8,29 @@ import boto3
 LOGGER = util.get_logger(__name__)
 
 
-def invoke_db_reader(read_query):
-    edge_db_reader_name = os.environ["EDGEDBReader"]
+def _invoke_lambda(function_name, query):
     lambda_client = boto3.client("lambda")
 
-    LOGGER.debug("Invoking the EDGE DB Reader . . .")
-
-    # Invoke the EDGE DB reader lambda to execute the query and to get the response
     db_read_lambda_response = lambda_client.invoke(
-        FunctionName=edge_db_reader_name, InvocationType='RequestResponse', LogType='None',
-        Payload=bytes(json.dumps(read_query).encode("utf-8"))
+        FunctionName=function_name, InvocationType='RequestResponse', LogType='None',
+        Payload=bytes(json.dumps(query).encode("utf-8"))
     )
 
     # Decode and return the lambda's response
     return json.loads(db_read_lambda_response["Payload"].read().decode("utf-8"))
 
 
+def invoke_db_reader(read_query):
+    edge_db_reader_name = os.environ["EDGEDBReader"]
+
+    LOGGER.debug("Invoking the EDGE DB Reader . . .")
+
+    return _invoke_lambda(edge_db_reader_name, read_query)
+
+
 def invoke_db_common_api(write_query):
     edge_common_api_name = os.environ["EDGECommonDBAPI"]
-    lambda_client = boto3.client("lambda")
 
     LOGGER.debug("Invoking the EDGE DB Common API . . .")
 
-    # Invoke the EDGE DB reader lambda to execute the query and to get the response
-    common_api_lambda_response = lambda_client.invoke(
-        FunctionName=edge_common_api_name, InvocationType='RequestResponse', LogType='None',
-        Payload=bytes(json.dumps(write_query).encode("utf-8"))
-    )
-
-    # Decode and return the lambda's response
-    return json.loads(common_api_lambda_response["Payload"].read().decode("utf-8"))
+    return _invoke_lambda(edge_common_api_name, write_query)
