f2e09c158fd3ef49a8907c3719e288efa80a21b4
ss350 <ss350@cummins.com>
2022-07-13 13:26:31 -0400

    SPAR-886: Fixed final BDD errors

--- EDGE-J1939-BDD/steps/j1939_fc_steps.py
+++ EDGE-J1939-BDD/steps/j1939_fc_steps.py
@@ -116,7 +116,7 @@ def assert_j1939_fc_message_in_ngdi(context):
     get_key = get_key_from_list_of_s3_objects(context.final_bucket, file_key)
     assert get_key is not None
     if get_key:
-        if os.path.isdir(DOWNLOAD_FOLDER_PATH):
+        if not os.path.isdir(DOWNLOAD_FOLDER_PATH):
             os.mkdir(DOWNLOAD_FOLDER_PATH)
         download_object_from_s3(context.final_bucket, get_key, context.download_ngdi_file_name)
         assert same_file_contents(context.compare_ngdi_file_name, context.download_ngdi_file_name) is True
