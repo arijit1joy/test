b3e1262d30723a21fccc3bd922ac4cfe653db57c
ss350 <ss350@cummins.com>
2022-07-18 13:39:39 -0400

    SPAR-884: Changed the log statements and added redundant catches

--- EdgeCPPTPoster/utilities/connection_utility.py
+++ EdgeCPPTPoster/utilities/connection_utility.py
@@ -1,36 +1,43 @@
 import json
 import os
-import utility as util
 
-import boto3
-
-LOGGER = util.get_logger(__name__)
+try:
+    import boto3
+except ModuleNotFoundError:
+    print("ERROR: Connection Util cannot import boto3")
 
 
 def _invoke_lambda(function_name, query):
-    lambda_client = boto3.client("lambda")
+    try:
+        lambda_client = boto3.client("lambda")
 
-    LOGGER.debug(f"Invoking Lambda {function_name}")
-    db_read_lambda_response = lambda_client.invoke(
-        FunctionName=function_name, InvocationType='RequestResponse', LogType='None',
-        Payload=bytes(json.dumps(query).encode("utf-8"))
-    )
+        db_read_lambda_response = lambda_client.invoke(
+            FunctionName=function_name, InvocationType='RequestResponse', LogType='None',
+            Payload=bytes(json.dumps(query).encode("utf-8"))
+        )
 
-    # Decode and return the lambda's response
-    return json.loads(db_read_lambda_response["Payload"].read().decode("utf-8"))
+        # Decode and return the lambda's response
+        return json.loads(db_read_lambda_response["Payload"].read().decode("utf-8"))
+    except NameError:
+        print("ERROR: boto3 has not been imported properly")
+        return None
+
+
 
 
 def invoke_db_reader(read_query):
+    if "EDGEDBReader_ARN" not in os.environ:
+        print("ERROR when connecting to DBReader: EDGEDBReader_ARN does not exist in os.environ!")
+        return None
+
     edge_db_reader_name = os.environ["EDGEDBReader_ARN"]
-
-    LOGGER.debug("Invoking the EDGE DB Reader Lambda . . .")
-
     return _invoke_lambda(edge_db_reader_name, read_query)
 
 
 def invoke_db_common_api(write_query):
+    if "EDGECommonDBAPI_ARN" not in os.environ:
+        print("ERROR when connecting to CommonAPI: EDGECommonDBAPI_ARN does not exist in os.environ!")
+        return None
+
     edge_common_api_name = os.environ["EDGECommonDBAPI_ARN"]
-
-    LOGGER.debug("Invoking the EDGE DB Common API Lambda . . .")
-
     return _invoke_lambda(edge_common_api_name, write_query)
