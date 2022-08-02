5422c87432e36ddca21b5a5f0054281127d650cb
tu118 <eric.g.hensley@cummins.com>
2022-08-01 23:41:33 -0400

    cleanup test case

--- EdgeCPPTPoster/tests/test_redis_utility.py
+++ EdgeCPPTPoster/tests/test_redis_utility.py
@@ -1,30 +1,10 @@
 import sys
-import boto3
-from unittest import TestCase
+import unittest
 from unittest.mock import patch, MagicMock
-from moto import mock_secretsmanager
 
 sys.path.append("../")
 sys.modules["edge_logger"] = MagicMock()
 sys.modules["rediscluster"] = MagicMock()
-
-MOCK_SECRETSMANAGER = mock_secretsmanager()
-SM_CLIENT = boto3.client('secretsmanager', region_name='us-east-1')
-REDIS_SECRET_NAME = "test_secret_name"
-
-
-def create_test_secret():
-    SM_CLIENT.create_secret(Name=REDIS_SECRET_NAME, SecretString='{"redis_host": "testing", "redis_port": "testing"}')
-
-
-def delete_test_secret():
-    SM_CLIENT.delete_secret(SecretId=REDIS_SECRET_NAME)
-
-MOCK_SECRETSMANAGER.start()
-print("<---Successfully started mocking secrets manager actions!--->")
-
-create_test_secret()
-print("<---Successfully created the test secret!--->")
 
 with patch.dict("os.environ", {'RedisSecretName': 'test_secret_name',
                                'region': 'us-east-1',
@@ -37,27 +17,11 @@ del sys.modules["edge_logger"]
 del sys.modules["rediscluster"]
 
 
-class TestRedisUtility(TestCase):
-    @classmethod
-    def tearDownClass(cls):
-        delete_test_secret()
-        print("<---Successfully deleted the test secret!--->")
-
-        MOCK_SECRETSMANAGER.stop()
-        print("<---Successfully stopped mocking secrets manager actions!--->")
-
-
-    # @patch.dict("os.environ",
-    # {'redis_secret_name': REDIS_SECRET_NAME, 'region': 'us-east-1', "LoggingLevel": "debug"})
-    # def test_getRedisConnection_whenGetRedisConnectionIsCalled_thenConnectionObjectIsReturned(self):
-    #     print("<---test_getRedisConnection_whenGetRedisConnectionIsCalled_thenConnectionObjectIsReturned--->")
-    #
-    #     result = get_redis_connection()
-    #
-    #     self.assertIsNotNone(result)
+class TestRedisUtility(unittest.TestCase):
 
     @patch("utilities.redis_utility.RedisCluster")
-    def test_getRedisConnection_whenGetRedisConnectionIsCalledAndExceptionOccurs_thenNoneIsReturned(self, mock_redis_cluster):
+    def test_getRedisConnection_whenGetRedisConnectionIsCalledAndExceptionOccurs_thenNoneIsReturned(self,
+                                                                                                    mock_redis_cluster):
         print("<---test_getRedisConnection_whenGetRedisConnectionIsCalledAndExceptionOccurs_thenNoneIsReturned--->")
         mock_redis_cluster.side_effect = Exception('Mock redis cluster exception')
         result = get_redis_connection()
@@ -112,3 +76,7 @@ class TestRedisUtility(TestCase):
         self.assertRaises(Exception)
         mock_get_redis_connection.assert_called()
         mock_read_from_the_edge_database.assert_not_called()
+
+
+if __name__ == '__main__':
+    unittest.main()
