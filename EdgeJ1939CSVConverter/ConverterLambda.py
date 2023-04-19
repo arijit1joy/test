4d5918c9f0c9a474b7d3fde39022a5ea440cbb91
Vishal Chaurasia <ua519@cummins.com>
2023-04-19 13:40:30 -0400

    bug fix json.loads

--- EdgeJ1939CSVConverter/ConverterLambda.py
+++ EdgeJ1939CSVConverter/ConverterLambda.py
@@ -29,7 +29,7 @@ EDGE_DB_CLIENT = EdgeDbLambdaClient()
 APP_ENV = os.environ["APPLICATION_ENVIRONMENT"]
 TABLE_NAME = os.environ["J1939ActiveFaultCodeTable"]
 # BDD_ESN = ["19299951", "19299955", "19299956", "19299952", "19299954", "CMMNS**19299954**************************************************************"]
-BDD_ESN = json.loads(ssm_client.get_parameter(Name='da-edge-j1939-bdd-esn-list', WithDecryption=False))
+BDD_ESN = ssm_client.get_parameter(Name='da-edge-j1939-bdd-esn-list', WithDecryption=False)
 BDD_ESN = BDD_ESN["esn"]
 
 def delete_message_from_sqs_queue(receipt_handle):
