ab1d5bb0d3d2bbc075775cb730a6b30787d57eea
ul711 <ul711@cummins.com>
2023-08-03 19:52:28 +0530

    CDP-1926: updated test case

--- EdgeCPPTPoster/tests/test_pcc_poster.py
+++ EdgeCPPTPoster/tests/test_pcc_poster.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+
 sys.path.append("../")
 import unittest
 from unittest.mock import patch, MagicMock, call
@@ -12,7 +13,6 @@ sys.modules["boto3"] = MagicMock()
 sys.modules["requests"] = MagicMock()
 sys.modules["kafka_producer"] = MagicMock()
 
-# sys.modules["kafka_producer"] = MagicMock()
 sys.modules["obfuscate_gps_utility"] = MagicMock()
 sys.modules["metadata_utility"] = MagicMock()
 sys.modules["utility"] = MagicMock()
@@ -31,26 +31,14 @@ with  CDAModuleMockingContext(sys) as cda_module_mock_context, patch.dict("os.en
     "j1939_stream_arn": "test"
 
 }):
-    # cda_module_mock_context.mock_module("edge_logger"),
-    # cda_module_mock_context.mock_module("sqs_send_message"),
     cda_module_mock_context.mock_module("kafka_producer.publish_message")
-    # cda_module_mock_context.mock_module("kafka_producer._create_kafka_message")
-    # cda_module_mock_context.mock_module("obfuscate_gps_utility.handle_gps_coordinates")
-    # cda_module_mock_context.mock_module("metadata_utility.write_health_parameter_to_database")
-    # cda_module_mock_context.mock_module(
-    #     "utility.util", mock_object=MagicMock(
-    #         get_logger=MagicMock(
-    #             return_value=(MagicMock(), "TestPTPoster",)
-    #         )
-    #     )
-    # )
     import pcc_poster
 
+
 class PCCPoster(unittest.TestCase):
-    post_url = "https://json"
-    headers = '{"Content-Type": "application/json", "Prefer": "param=single-object", "x-api-key": ""}'
     json_body = {"messageFormatVersion": "1.1.1", "telematicsPartnerName": "Cummins", "customerReference": "Cummins",
-                 "componentSerialNumber": "CMMNS**19299954**************************************************************",
+                 "componentSerialNumber": "CMMNS**1929992*******************"
+                                          "*******************************************",
                  "equipmentId": "EDGE_19299954", "vin": "TESTVIN19299954", "telematicsDeviceId": "192999999999954",
                  "dataSamplingConfigId": "Event1_5", "dataEncryptionSchemeId": "ES1", "numberOfSamples": 1, "samples": [
             {"convertedDeviceParameters": {"CPU_Usage_Level": "2.02", "LTE_RSRP": "-107", "LTE_RSRQ": "-6",
@@ -76,31 +64,10 @@ class PCCPoster(unittest.TestCase):
              }
         ]
                  }
-    j1939_data_type = "FC"
-    j1939_type = "FC"
-    file_uuid = '88123123123'
-
-    device_id = '192999999999954'
-
-    config_spec_and_req_id = "1234"
-
-    file_name = 'TestKafka'
-    file_size = '10'
-    esn = "CMMNS**19299954**************************************************************"
-    sqs_message_template = \
-        f"{file_uuid},{device_id},{file_name},{str(file_size)}," \
-        f"{'{FILE_METADATA_CURRENT_DATE_TIME}'},{j1939_data_type}," \
-        f"{'{FILE_METADATA_FILE_STAGE}'},{esn},{config_spec_and_req_id}"
-
-    headers_json = {
-        "SecretString": {
-            "x-api-key": "12345"
-        }
-    }
     hb_param_json = {"CPU_Usage_Level": "2.02", "LTE_RSRP": "-107", "LTE_RSRQ": "-6",
                      "messageID": "8c5a1650-048e-4620-9950-bc14fa4b46b7", "Latitude": "39.202938",
                      "CPU_temperature": "40.3", "Satellites_Used": "20", "Longitude": "-85.88672",
-                     "PDOP": "1.159999967", "LTE_RSCP": "255", "PMIC_temperature": "33.25", "LTE_RSSI": "99",
+                     "PDOP": "1.159999967", "LTE_RSCP": "253", "PMIC_temperature": "33.25", "LTE_RSSI": "99",
                      "Altitude": "165.236"
                      }
 
@@ -117,6 +84,7 @@ class PCCPoster(unittest.TestCase):
         assert call().put_record(StreamARN='test', Data=json.dumps(self.json_body, indent=2).encode('utf-8'),
                                  PartitionKey='123456789-J1939-Data') in mock_client.mock_calls
         pcc_poster.sqs_send_message.assert_called()
+
 
 if __name__ == '__main__':
     unittest.main()
