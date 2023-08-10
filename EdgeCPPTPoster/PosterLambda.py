5e2a715885782dccec0433f6f24c8ffff17a281b
Deepti Spencer <sy072@cummins.com>
2023-08-10 10:08:34 -0400

    Jfrog imports

--- EdgeCPPTPoster/PosterLambda.py
+++ EdgeCPPTPoster/PosterLambda.py
@@ -3,19 +3,24 @@ import json
 import time
 import uuid
 import boto3
-import requests
-import post
-import pt_poster
-import pcc_poster
-import utility as util
-import environment_params as env
-from multiprocessing import Process
-from edge_sqs_utility_layer.sqs_utility import sqs_send_message
-import sys
-from update_scheduler import update_scheduler_table, get_request_id_from_consumption_view
+import traceback
+try:
+    import requests
+    import post
+    import pt_poster
+    import pcc_poster
+    import utility as util
+    import environment_params as env
+    from multiprocessing import Process
+    from edge_sqs_utility_layer.sqs_utility import sqs_send_message
+    import sys
+    from update_scheduler import update_scheduler_table, get_request_id_from_consumption_view
 
-sys.path.insert(1, './lib')
-from edge_db_lambda_client import EdgeDbLambdaClient
+    sys.path.insert(1, './lib')
+    from edge_db_lambda_client import EdgeDbLambdaClient
+except Exception as e:
+    traceback.print_exc()
+    raise e
 
 LOGGER = util.get_logger(__name__)
 
