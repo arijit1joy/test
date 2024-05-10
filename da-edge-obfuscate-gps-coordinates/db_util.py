4b07e345c71be2b3e8defcd0ac84823c5a13368e
Sravan Alampalle <sravan.kumar.alampalle@cummins.com>
2024-05-10 12:15:44 +0530

    IIE-411 : Obfuscate Lambda changes

---
+++ da-edge-obfuscate-gps-coordinates/db_util.py
@@ -0,0 +1,113 @@
+import utility as util
+from pypika import Query, Table
+import time
+import os
+import json
+import boto3
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+time_format = os.getenv('TimeFormat')
+
+logger = util.get_logger(__name__)
+
+region = os.getenv('region')
+secret_name = os.environ['EdgeRDSSecretName']
+session = boto3.session.Session()
+client = session.client(service_name='secretsmanager', region_name=region)
+get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+secret_string = get_secret_value_response['SecretString']
+secret_params = json.loads(secret_string)
+
+
+def get_certification_family(device_id, esn):
+    query = get_certification_family_query(device_id, esn)
+    try:
+        response = db_request("get", query)
+        if len(response) == 0:
+            return ""
+        return response[0]
+    except Exception as e:
+        logger.info("Unable to get certification family information from device_information table")
+        return server_error(str(e))
+
+
+def get_certification_family_query(device_id, esn):
+    device_information = Table('device_information', schema='da_edge_olympus')
+    query = Query.from_(device_information)\
+                 .select(device_information.certification_family)\
+                 .where(device_information.engine_serial_number == esn)\
+                 .where(device_information.device_id == device_id)
+
+    logger.debug(query.get_sql(quote_char=None))
+    return query.get_sql(quote_char=None)
+
+
+def insert_into_metadata_Table(device_id, message_id, file_name, file_size, esn, config_id):
+    query = insert_to_metadata_table_query(device_id, message_id, file_name, file_size, esn, config_id)
+    try:
+        db_request("post", query)
+    except Exception as e:
+        logger.info("Unable to insert into metadata table")
+        return server_error(str(e))
+
+
+def insert_to_metadata_table_query(device_id, message_id, file_name, file_size, esn, config_id):
+    time_default_format = time.localtime()
+    current_date_time = time.strftime(time_format, time_default_format)
+    da_edge_metadata = Table('da_edge_olympus.da_edge_metadata')
+    query = Query.into(da_edge_metadata).columns(da_edge_metadata.device_id,
+                                                 da_edge_metadata.uuid,
+                                                 da_edge_metadata.data_protocol,
+                                                 da_edge_metadata.data_pipeline_stage,
+                                                 da_edge_metadata.esn,
+                                                 da_edge_metadata.config_spec_name,
+                                                 da_edge_metadata.created_datetime)
+    query = query.insert(device_id, message_id, 'J1939_FC', 'FILE_RECEIVED', esn, config_id, current_date_time)
+    logger.info(query.get_sql(quote_char=None))
+    return query.get_sql(quote_char=None)
+
+
+def db_request(method, query):
+
+    conn = psycopg2.connect(get_db_connection_string())
+
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute(query)
+    if method == 'get':
+        response = json.loads(json.dumps(cur.fetchall(), indent=2))
+        logger.info(response)
+        return response
+
+    conn.commit()
+    body = {
+        "message": "operation successful"
+    }
+
+    return body
+
+
+def get_db_connection_string():
+    db_conn_string = "host=" + secret_params['host'] + " port=" + str(secret_params['port']) + " dbname=" + secret_params['dbname'] + " user=" + \
+                     secret_params['username'] + " password=" + secret_params['password']
+    return db_conn_string
+
+
+def server_error(message):
+    body = {
+        "errorMessage": message
+    }
+    return __http_response(500, body)
+
+
+def __http_response(status_code, body):
+    response = {
+            "isBase64Encoded": False,
+            "statusCode": status_code,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": json.dumps(body)
+        }
+    return response
--- da-edge-obfuscate-gps-coordinates/obfuscate_gps_handler.py
+++ da-edge-obfuscate-gps-coordinates/obfuscate_gps_handler.py
@@ -4,6 +4,8 @@ import boto3
 import utility as util
 from datetime import datetime
 from edge_db_utility_layer.obfuscate_gps_utility import handle_gps_coordinates
+from db_util import get_certification_family
+from db_util import insert_into_metadata_Table
 
 LOGGER = util.get_logger(__name__)
 
@@ -21,6 +23,12 @@ def obfuscate_gps(body):
                         handle_gps_coordinates(latitude, longitude)
                     LOGGER.info(f"Latitude: {converted_device_params['Latitude']}, "
                                 f"Longitude: {converted_device_params['Longitude']}, after obfuscated gps coordinates")
+            # Depending on config_id insert to metadata table & get Certification Family information
+            config_id = body["dataSamplingConfigId"]
+            if config_id.startswith('SC9XXX') or config_id.startswith('DC9XXX') or config_id.startswith('DS9XXX'):
+                insert_into_metadata_Table(body["telematicsDeviceId"], sample["messageID"], body["componentSerialNumber"], config_id)
+                certificationFamily = get_certification_family(body["telematicsDeviceId"], body["componentSerialNumber"])
+                body["certificationFamily"] = certificationFamily
     send_file_to_s3(body)
 
 
@@ -28,6 +36,7 @@ def send_file_to_s3(body):
     try:
         s3_client = boto3.client("s3")
         bucket_name = os.environ["j1939_end_bucket"]
+        emission_bucket_name = os.environ["j1939_emission_end_bucket"]
         device_id = body["telematicsDeviceId"]
 
         esn = body["componentSerialNumber"]
@@ -48,7 +57,12 @@ def send_file_to_s3(body):
         file_key = "ConvertedFiles/{0}/{1}/{2}/{3}/{4}/{5}".format(
             esn, device_id, current_dt.strftime("%Y"), current_dt.strftime("%m"), current_dt.strftime("%d"), file_name)
         LOGGER.info(f"File Name: {file_name}, File Key:  {file_key}")
-        send_to_s3_response = s3_client.put_object(Bucket=bucket_name, Key=file_key, Body=json.dumps(body).encode())
+
+        # Depending on config_id insert to emission bucket else insert to j1939 bucket
+        if config_id.startswith('SC9XXX') or config_id.startswith('DC9XXX') or config_id.startswith('DS9XXX'):
+            send_to_s3_response = s3_client.put_object(Bucket=emission_bucket_name, Key=file_key, Body=json.dumps(body).encode())
+        else:
+            send_to_s3_response = s3_client.put_object(Bucket=bucket_name, Key=file_key, Body=json.dumps(body).encode())
         LOGGER.debug(f"Send to S3 Response: {send_to_s3_response}")
     except Exception as e:
         LOGGER.error(f"An error occurred while sending file to s3:  {e}")
