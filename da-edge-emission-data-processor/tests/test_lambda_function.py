89bba7333e33b2bd87107e504ffc279a7ce6cb51
Sravan Alampalle <sravan.kumar.alampalle@cummins.com>
2024-06-03 16:28:40 +0530

    IIE-452 : testcase fix 2

--- da-edge-emission-data-processor/tests/test_lambda_function.py
+++ da-edge-emission-data-processor/tests/test_lambda_function.py
@@ -24,7 +24,7 @@ class TestLambdaFunction(TestCase):
     @patch('lambda_function.insert_into_metadata_Table')
     def test_lambda_handler(self, mock_insert_into_metadata_Table, mock_push_to_tsb, mock_get_content):
         event = {"Records": [{"body": str("{\"Records\": [{\"s3\": {\"object\": {\"key\": \"/fileKey\"}}}]}")}]}
-        mock_get_content.return_value = "{\"telematicsDeviceId\": \"357649072115903\", \"componentSerialNumber\": \"64505184\", \"dataSamplingConfigId\": \"SC9110\" }"
+        mock_get_content.return_value = "{\"telematicsDeviceId\": \"357649072115903\", \"componentSerialNumber\": \"64505184\", \"dataSamplingConfigId\": \"SC9110\" }", "1137167d-e18a-4830-bdc6-a5a15d14d61b "
         mock_push_to_tsb.return_value = None
         mock_insert_into_metadata_Table.return_value = None
         lambda_handler(event, None)
@@ -37,7 +37,7 @@ class TestLambdaFunction(TestCase):
     @patch('lambda_function.insert_into_metadata_Table')
     def test_lambda_handler02(self, mock_insert_into_metadata_Table, mock_push_to_tsb, mock_get_content):
         event = {"Records": [{"body": str("{\"Records\": [{\"s3\": {\"object\": {\"key\": \"/fileKey\"}}}]}")}]}
-        mock_get_content.return_value = "{\"componentSerialNumber\": \"64505184\", \"dataSamplingConfigId\": \"SC9110\" }"
+        mock_get_content.return_value = "{\"componentSerialNumber\": \"64505184\", \"dataSamplingConfigId\": \"SC9110\" }", "1137167d-e18a-4830-bdc6-a5a15d14d61b"
         mock_push_to_tsb.return_value = None
         lambda_handler(event, None)
         mock_get_content.assert_called()
