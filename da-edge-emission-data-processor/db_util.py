5e1a7ee90779aafce482d495e8c1f72be0199bd5
Akshay Champavat <akshayrajsinh.champavat@cummins.com>
2024-06-11 14:52:32 +0530

    added datarx in progress part for emission

--- da-edge-emission-data-processor/db_util.py
+++ da-edge-emission-data-processor/db_util.py
@@ -1,7 +1,7 @@
 import time
 
 import utility as util
-from pypika import Query, Table
+from pypika import Query, Table,functions as fn
 import os
 
 from edge_core_layer.edge_core import api_request
@@ -42,3 +42,107 @@ def insert_to_metadata_table_query(device_id, message_id, esn, config_id, file_n
     query = query.insert(device_id, message_id, 'J1939_Emissions', 'FILE_SENT', esn, config_id, file_name, file_size, current_date_time, current_date_time)
     logger.info(query.get_sql(quote_char=None))
     return query.get_sql(quote_char=None)
+
+
+def get_request_id_from_consumption_view(data_protocol, data_config_filename):
+    query = _get_request_id_query(data_protocol, data_config_filename)
+    try:
+        response = api_request(edgeCommonAPIURL, "GET", query)
+        logger.info(f" getting requestID - {response[0]['request_id']}")
+        return response[0]['request_id'], response[0]['status']
+    except Exception as e:
+        logger.info("Error in getting request ID")
+        return server_error(str(e))
+
+
+def _get_request_id_query(data_type, data_config_filename):
+    data_requester_information = Table('da_edge_olympus.data_requester_information')
+    scheduler = Table('da_edge_olympus.scheduler')
+    split_config_filename = data_config_filename.split("_")
+    query = Query.from_(data_requester_information) \
+        .join(scheduler) \
+        .on(data_requester_information.request_id == scheduler.request_id) \
+        .select(scheduler.request_id, scheduler.status) \
+        .where(data_requester_information.data_type == data_type) \
+        .where(scheduler.device_id == split_config_filename[1]) \
+        .where(scheduler.engine_serial_number == split_config_filename[2]) \
+        .where(fn.Substring(scheduler.config_spec_file_name, 1, 6) == split_config_filename[3]) \
+        .where(scheduler.status.isin(['Config Accepted', 'Data Rx In Progress']))
+    logger.info(f"Query for getting request ID- {query}")
+    return query.get_sql(quote_char=None)
+
+def update_scheduler_table(req_id, device_id, config_status):
+    if req_id and config_status != "Data Rx In Progress":
+        try:
+            current_scedhuler_status =get_current_scheduler_data_requester_status(req_id,device_id)
+            query = get_update_scheduler_query(req_id, device_id)
+            logger.debug(f"Update Scheduler Table Query: {query}")
+            response = api_request(edgeCommonAPIURL, "POST", query)
+            logger.info('Successfully updated scheduler table with Data Rx In Progress ')
+            # inserting to audit_data_request_status table
+            insert_into_audit_data_request_status_table(req_id, device_id, current_scedhuler_status, 'Data Rx In Progress')
+
+        except Exception as e:
+            logger.error(f"Failed to update scheduler table with Data Rx In Progress: '{e}'")
+    else:
+        logger.info(f"Could not get the Request ID for this file or the config_status was Data Rx In Progress!")
+
+
+def get_update_scheduler_query(req_id, device_id):
+    time_format = '%Y-%m-%d %H:%M:%S'
+    time_default_format = time.localtime()
+    current_date_time = time.strftime(time_format, time_default_format)
+    scheduler = Table('da_edge_olympus.scheduler')
+    query = Query.update(scheduler).set(scheduler.status, 'Data Rx In Progress').set(scheduler.updated_date_time,
+                                                                                     current_date_time).where(
+        scheduler.request_id == req_id
+        ).where(scheduler.device_id == device_id
+                ).where(scheduler.status == 'Config Accepted')
+    logger.info(f"Query for updating scheduler table with Data Rx In Progress - {query}")
+    return query.get_sql(quote_char=None)
+
+
+def get_cspec_req_id(sc_number):
+    if '-' in sc_number:
+        config_spec_name = ''.join(sc_number.split('-')[:-1])
+        req_id = sc_number.split('-')[-1]
+    else:
+        config_spec_name = sc_number
+        req_id = None
+    return config_spec_name, req_id
+
+
+def get_current_scheduler_data_requester_status(request_id, device_id):
+    scheduler = Table('da_edge_olympus.SCHEDULER')
+    data_requester_information = Table('da_edge_olympus.data_requester_information')
+    query = Query.from_(scheduler
+                        ).inner_join(data_requester_information
+                                     ).on(scheduler.request_id == data_requester_information.request_id
+                                          )
+    query = query.select(scheduler.request_id, scheduler.status).where(scheduler.request_id == request_id
+                                 ).where(scheduler.device_id == device_id).get_sql(quote_char=None)
+    try:
+        response = api_request(edgeCommonAPIURL, "GET", query)
+        if response:
+            logger.info(f" getting current status of request - {response[0]['status']}")
+            return response[0]['status']
+    except Exception as exception:
+        logger.error(f"Error couldn't get current scheduler status: {exception}")
+        return ""
+
+def insert_into_audit_data_request_status_table(request_id, device_id, old_status, new_status):
+    audit_dnr_table = Table('da_edge_olympus.audit_data_request_status')
+    query = Query.into(audit_dnr_table).columns(
+        audit_dnr_table.request_id,
+        audit_dnr_table.device_id,
+        audit_dnr_table.old_status,
+        audit_dnr_table.new_status,
+        audit_dnr_table.status_from_oem
+    ).insert(request_id, device_id, old_status, new_status).get_sql(quote_char=None)
+    logger.info(f"Insert audit_data_request_status Table Query: {query}")
+    try:
+        response = api_request(edgeCommonAPIURL, "POST", query)
+        logger.info(f"successfully updated audit_data_request_status table : {response}")
+    except Exception as exception:
+        logger.error(f"Error updating audit_data_request_status table: {str(exception)}")
+
--- da-edge-emission-data-processor/lambda_function.py
+++ da-edge-emission-data-processor/lambda_function.py
@@ -4,7 +4,7 @@ import json
 sys.path.insert(1, './lib')
 import utility as util
 from s3_util import get_content
-from db_util import insert_into_metadata_Table
+import db_util
 from tsb_util import push_to_tsb
 
 LOGGER = util.get_logger(__name__)
@@ -22,10 +22,16 @@ def lambda_handler(event, context):
                 # download file in s3 bucket
                 content, uuid = get_content(key)
                 content_json = json.loads(content)
+                # Get the Request ID
+                config_spec_name, req_id = db_util.get_cspec_req_id(content_json['dataSamplingConfigId'])
+                data_config_filename = '_'.join(['EDGE', content_json["telematicsDeviceId"], content_json["componentSerialNumber"], config_spec_name])
+                request_id, schedular_status = db_util.get_request_id_from_consumption_view('J1939_Emissions', data_config_filename)
+                # Updating the scheduler table to data rx in progress
+                db_util.update_scheduler_table(request_id , content_json["telematicsDeviceId"] , schedular_status)
                 # Send to AAI Cloud
                 push_to_tsb(content_json)
                 # insert a row in metadata table to FILE_SENT
-                insert_into_metadata_Table(content_json["telematicsDeviceId"], uuid, content_json["componentSerialNumber"], content_json['dataSamplingConfigId'],
+                db_util.insert_into_metadata_Table(content_json["telematicsDeviceId"], uuid, content_json["componentSerialNumber"], content_json['dataSamplingConfigId'],
                                            key.rsplit('/', 1).pop(), len(content))
     except Exception as e:
         LOGGER.error(f"An error occurred while processing Emission data: {e}")
